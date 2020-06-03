004befafd4d0f8c6c80c5dde3696671ee91dab22
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-03 15:45:33 +0200

    update regex to include common symbols

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -7,7 +7,7 @@ router.post('/caesar', (req, res, next) => {
   const factor = req.body.factor
   console.log(`${text} - ${factor}`)
 
-  if (/^[a-zA-Z\s]*$/.test(text) === false) {
+  if (/^[a-zA-Z,.!?\s]*$/.test(text) === false) {
     res.json({
       error: 'Your text contains forbidden characters! Please use only letters from A to Z.',
     })
