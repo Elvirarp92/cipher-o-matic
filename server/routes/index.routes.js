dc6adcef2f7429ddabfec7e8f214f67bb401e390
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-03 21:28:59 +0200

    add direction to the /caesar endpoint

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -5,13 +5,14 @@ const { caesar } = require('./../configs/functions.config')
 router.post('/caesar', (req, res, next) => {
   const text = req.body.text
   const factor = req.body.factor
+  const direction = req.body.direction
 
   if (/^[a-zA-Z,.!?'\s]*$/.test(text) === false) {
     res.json({
       error: 'Your text contains forbidden characters! Please use only letters from A to Z, commas, periods, apostrophes, interrogation and exclamation.',
     })
   } else {
-    res.json({ message: caesar(text, factor) })
+    res.json({ message: caesar(text, factor, direction) })
   }
 })
 
