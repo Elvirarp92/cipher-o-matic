b3fed7b6f6059c2b630ff7715ad558fe041702f9
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-03 21:16:08 +0200

    add encryption/decryption option to the Caesar cipher function, to be added to route

--- server/configs/functions.config.js
+++ server/configs/functions.config.js
@@ -3,20 +3,49 @@ const max = 'Z'.charCodeAt(0)
 const lowerMin = 'a'.charCodeAt(0)
 const lowerMax = 'z'.charCodeAt(0)
 
+function charCaesarEncrypt(char, factor, min) {
+  const letterPos = char.charCodeAt(0) % min
+
+  const encryptedLetterPos = letterPos + factor
+
+  const encryptedCharCode = (encryptedLetterPos % 26) + min
+
+  return String.fromCharCode(encryptedCharCode)
+}
+
+function charCaesarDecrypt(char, factor, min) {
+  const decryptedCharCode = char.charCodeAt(0) - factor
+
+  return decryptedCharCode < min
+    ? String.fromCharCode(decryptedCharCode + 26)
+    : String.fromCharCode(decryptedCharCode)
+}
+
+//There is a ton of repetition here. There should be a way to make this code DRYer.
+
 module.exports = {
-  caesar: (text, factor) => {
+  caesar: (text, factor, direction) => {
     let cipher = ''
-    for (let i = 0; i < text.length; i++) {
-      text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90
-        ? (cipher += String.fromCharCode(
-            ((text.charCodeAt(i) - min + factor) % (max - min + 1)) + min
-          ))
-        : text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122
-        ? (cipher += String.fromCharCode(
-            ((text.charCodeAt(i) - lowerMin + factor) % (lowerMax - lowerMin + 1)) + lowerMin
-          ))
-        : (cipher += text[i])
+    if (direction === 'encryption') {
+      for (let i = 0; i < text.length; i++) {
+        text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90
+          ? (cipher += charCaesarEncrypt(text[i], factor, min))
+          : text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122
+          ? (cipher += charCaesarEncrypt(text[i], factor, lowerMin))
+          : (cipher += text[i])
+      }
+    } else if (direction === 'decryption') {
+      for (let i = 0; i < text.length; i++) {
+        if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+          cipher += charCaesarDecrypt(text[i], factor, min)
+        } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+          cipher += charCaesarDecrypt(text[i], factor, lowerMin)
+        } else {
+          cipher += text[i]
+        }
+      }
     }
+
     return cipher
   },
 }
--- server/tests/functions.test.js
+++ server/tests/functions.test.js
@@ -1,17 +1,33 @@
 const { caesar } = require('./../configs/functions.config')
 
 test('Ciphers the whole alphabet to ROT13, equalling "NOPQRSTUVWXYZABCDEFGHIJKLM"', () => {
-  expect(caesar('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 13)).toBe('NOPQRSTUVWXYZABCDEFGHIJKLM')
+  expect(caesar('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 13, 'encryption')).toBe('NOPQRSTUVWXYZABCDEFGHIJKLM')
 })
 
 test('Ciphers "ABCXYZ" to ROT13, equalling "NOPKLM"', () => {
-  expect(caesar('ABCXYZ', 13)).toBe('NOPKLM')
+  expect(caesar('ABCXYZ', 13, 'encryption')).toBe('NOPKLM')
 })
 
 test('Ciphers "Lorem Ipsum Dolor Sit Amet" to "Yberz Vcfhz Qbybe Fvg Nzrg" on ROT13', () => {
-  expect(caesar('Lorem Ipsum Dolor Sit Amet', 13)).toBe('Yberz Vcfhz Qbybe Fvg Nzrg')
+  expect(caesar('Lorem Ipsum Dolor Sit Amet', 13, 'encryption')).toBe('Yberz Vcfhz Qbybe Fvg Nzrg')
 })
 
 test('Ciphers "Lorem Ipsum Dolor Sit Amet" to "Twzmu Qxacu Lwtwz Aqb Iumb" on ROT8', () => {
-  expect(caesar('Lorem Ipsum Dolor Sit Amet', 8)).toBe('Twzmu Qxacu Lwtwz Aqb Iumb')
+  expect(caesar('Lorem Ipsum Dolor Sit Amet', 8, 'encryption')).toBe('Twzmu Qxacu Lwtwz Aqb Iumb')
+})
+
+test('Decrypts "NOPQRSTUVWXYZABCDEFGHIJKLM" as ROT13, plaintext being the whole alphabet', () => {
+  expect(caesar('NOPQRSTUVWXYZABCDEFGHIJKLM', 13, 'decryption')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+})
+
+test('Decrypts "NOPKLM" in ROT13 to "ABCXYZ"', () => {
+  expect(caesar('NOPKLM', 13, 'decryption')).toBe('ABCXYZ')
+})
+
+test('Decrypts "Yberz Vcfhz Qbybe Fvg Nzrg" on ROT13 to "Lorem Ipsum Dolor Sit Amet"', () => {
+  expect(caesar('Yberz Vcfhz Qbybe Fvg Nzrg', 13, 'decryption')).toBe('Lorem Ipsum Dolor Sit Amet')
+})
+
+test('Decrypts "Twzmu Qxacu Lwtwz Aqb Iumb" on ROT8 to "Lorem Ipsum Dolor Sit Amet"', () => {
+  expect(caesar('Twzmu Qxacu Lwtwz Aqb Iumb', 8, 'decryption')).toBe('Lorem Ipsum Dolor Sit Amet')
 })
