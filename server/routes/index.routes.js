a694a11bcd52e33441ba6357d2a30deea98ddd59
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-03 21:29:47 +0200

    fix regex to include Spanish interrogation and exclamation signs

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -7,7 +7,7 @@ router.post('/caesar', (req, res, next) => {
   const factor = req.body.factor
   const direction = req.body.direction
 
-  if (/^[a-zA-Z,.!?'\s]*$/.test(text) === false) {
+  if (/^[a-zA-Z,.¡¿!?'\s]*$/.test(text) === false) {
     res.json({
       error: 'Your text contains forbidden characters! Please use only letters from A to Z, commas, periods, apostrophes, interrogation and exclamation.',
     })
